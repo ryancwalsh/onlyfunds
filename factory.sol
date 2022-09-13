a7e0bf4cbf7f02c26145111aed574e0a9fc298ff
Derrek <80121818+derrekcoleman@users.noreply.github.com>
2022-09-13 12:33:04 +0100

    ship changes to main

--- factory.sol
+++ factory.sol
@@ -1,26 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
-import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+import "./token.sol";
 
 contract MyToken is ERC20, Ownable {
-
     // ATTS
-    
-        uint256 public totalFunds = 0;
-        uint public timeToDeadline = 30 days; 
-        //uint public fundraiseStartTime = block.timestamp;
-        uint public fundraiseStartTime = 70 days; 
-        uint public fundraiseEndTime = fundraiseStartTime + timeToDeadline;
 
     constructor() ERC20("MyToken", "MTK") {}
 
     // METHODS
-        
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
-        }
 
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 
+    function create(address from, uint256 amount) public onlyOwner {
+        Project project = new Project();
+    }
 }
--- project.sol
+++ project.sol
@@ -4,54 +4,47 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
 
-contract Payable {
-    // Payable address can receive Ether
-    address payable public owner;
+contract Project is ERC20, Ownable {
+    //implement "changeOwner" so the Factory isn't the owner anymore
 
     // ATTS
 
-        // this can be set by the factory?
-        uint public timeToDeadline = 30 days; 
-        // this should be set by the factory
-        // uint public fundraiseStartTime = block.timestamp;
-        uint public fundraiseStartTime = 70 days; 
-        uint public fundraiseEndTime = fundraiseStartTime + timeToDeadline;
+    // this can be set by the factory?
+    uint public timeToDeadline = 30 days;
+    // this should be set by the factory
+    // uint public fundraiseStartTime = block.timestamp;
+    uint public fundraiseStartTime = 70 days;
+    uint public fundraiseEndTime = fundraiseStartTime + timeToDeadline;
 
     // Payable constructor can receive Ether
-    constructor() payable {
-        owner = payable(msg.sender);
+    constructor(address ownerAddress, uint) {
+        transferOwnership(ownerAddress);
     }
 
     // METHODS
 
-        function donate() public payable {
-            if (fundraiseStartTime + timeToDeadline < block.timestamp) {
+    function donate() public payable {
+        if (fundraiseStartTime + timeToDeadline < block.timestamp) {
             // give donator DAO Token
             _mint(msg.sender, msg.value);
             totalFunds += msg.value;
-            }
         }
+    }
 
-        // Function to withdraw all Ether from this contract.
-        function withdrawInvestment() public onlyOwner {
+    // Function to withdraw all Ether from this contract.
+    function withdrawInvestment() public onlyOwner {
         // get the amount of Ether stored in this contract
-            uint amount = address(this).balance;
+        uint amount = address(this).balance;
 
-            // send all Ether to owner
-            // Owner can receive Ether since the address of owner is payable
-            // --- how accurate is the timestamp thing?
-            if (fundraiseStartTime + timeToDeadline < block.timestamp)
+        // send all Ether to owner
+        // Owner can receive Ether since the address of owner is payable
+        // --- how accurate is the timestamp thing?
+        if (fundraiseStartTime + timeToDeadline < block.timestamp)
             (bool success, ) = owner.call{value: amount}("");
-            require(success, "Failed to withdraw");
-        }
+        require(success, "Failed to withdraw");
+    }
 
-        function withdrawDAO() public {
+    function withdrawDAO() public {}
 
-        }
-
-
-        function stopProject() public onlyOwner {
-            
-        }
-
+    function stopProject() public onlyOwner {}
 }
