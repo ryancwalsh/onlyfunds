acc5c3da724ff004ad4ef879bad2a6656fa61b08
Patrick Gerard <info@content-baer.de>
2022-09-13 11:32:34 +0100

    split smartcontracts into two files

--- factory.sol
+++ factory.sol
@@ -5,33 +5,22 @@ import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    uint256 public totalFunds = 0;
-    uint public timeToDeadline = 30 days; 
-    //uint public fundraiseStartTime = block.timestamp;
-    uint public fundraiseStartTime = 70 days; 
-    uint public fundraiseEndTime = fundraiseStartTime + timeToDeadline;
+
+    // ATTS
+    
+        uint256 public totalFunds = 0;
+        uint public timeToDeadline = 30 days; 
+        //uint public fundraiseStartTime = block.timestamp;
+        uint public fundraiseStartTime = 70 days; 
+        uint public fundraiseEndTime = fundraiseStartTime + timeToDeadline;
 
     constructor() ERC20("MyToken", "MTK") {}
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
-
-    // do we need totalFunds when we use a payable contract
-    function donate() public payable{
-        if (block.timestamp < fundraiseEndTime) {
-        _mint(msg.sender, msg.value);
-        totalFunds += msg.value;
+    // METHODS
+        
+        function mint(address to, uint256 amount) public onlyOwner {
+            _mint(to, amount);
         }
-    }
 
-    function withdrawFunds() public onlyOwner {
-        payable(msg.sender).transfer(address(this).balance);
-        totalFunds = 0;
-    }
-
-    function unlock() public {
-
-    }
 
 }
